392b03579e997fbddb2840e9266aa62d2dd77d70
Timur Rustamov <tsimur.rustamov@gmail.com>
2021-10-07 12:48:38 +0000

    last changes

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,29 @@
 #! /bin/bash
 
+echo "--------------------------"
 print_title(){
 	echo Deploy Script
 }
-
+echo "--------------------------"
 print_date(){
-	echo Date: $(date)
+	echo Date: $(date +'%d-%m-%Y %H-%M-%S')
 }
+echo "--------------------------"
 # pass package name to function
 install_package(){
 	sudo apt-get install -y $1
 }
+echo "--------------------------"
 apt_update(){
 	sudo apt-get update
 }
+echo "--------------------------"
 remove_old_site(){
 	if [ ! -d /var/www/html/.git ]; then
           sudo rm -f /var/www/html/index.html
 	fi
 }
+echo "--------------------------"
 clone_website_code(){
 	if [ ! -d /var/www/html/.git ]; then
 	  sudo git clone https://github.com/octocat/Spoon-Knife /var/www/html/
@@ -30,6 +35,7 @@ pull_latest(){
 	  git pull https://github.com/octocat/Spoon-Knife
 	fi
 }
+echo "--------------------------"
 
 print_title
 print_date
