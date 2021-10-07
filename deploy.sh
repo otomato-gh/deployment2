2e88bbc9c4fadad9b3ece7be84dc9edb9aefa624
Ihar Yefimenka <iharye@playtika.com>
2021-10-07 12:57:14 +0000

    final fixes

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,23 @@
 #! /bin/bash
 
-echo "--------------------------"
 print_title(){
 	echo Deploy Script
 }
-echo "--------------------------"
 print_date(){
 	echo Date: $(date +'%d-%m-%Y %H-%M-%S')
 }
-echo "--------------------------"
 # pass package name to function
 install_package(){
 	sudo apt-get install -y $1
 }
-echo "--------------------------"
 apt_update(){
 	sudo apt-get update
 }
-echo "--------------------------"
 remove_old_site(){
 	if [ ! -d /var/www/html/.git ]; then
           sudo rm -f /var/www/html/index.html
 	fi
 }
-echo "--------------------------"
 clone_website_code(){
 	if [ ! -d /var/www/html/.git ]; then
 	  sudo git clone https://github.com/octocat/Spoon-Knife /var/www/html/
@@ -35,7 +29,6 @@ pull_latest(){
 	  git pull https://github.com/octocat/Spoon-Knife
 	fi
 }
-echo "--------------------------"
 
 apache_check(){
 
@@ -52,10 +45,20 @@ apache_check(){
 	}
 
 print_title
+echo "----------------------"
 print_date
+echo "----------------------"
 apt_update
+echo "----------------------"
 install_package git
+echo "----------------------"
 install_package apache2
+echo "----------------------"
 remove_old_site
+echo "----------------------"
 clone_website_code
+echo "----------------------"
 pull_latest
+echo "----------------------"
+apache_check
+echo "----------------------"
